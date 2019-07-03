ddda6061bc912ae6f23589078b116a73cee7cf24
Corey Durthaler <cdurth@gmail.com>
2019-07-02 21:58:31 -0400

    cleanup

--- dotnetcorestarter/ClientApp/containers/Login/actions.js
+++ dotnetcorestarter/ClientApp/containers/Login/actions.js
@@ -13,9 +13,6 @@
             .then(data => {
                 console.log(data)
                 if (data.message) {
-                    // Here you should have logic to handle invalid creation of a user.
-                    // This assumes your Rails API will return a JSON object with a key of
-                    // 'message' if there is an error with creating the user, i.e. invalid username
                 } else {
                     localStorage.setItem("token", data.jwt)
                     dispatch(loginUser(data.user))
